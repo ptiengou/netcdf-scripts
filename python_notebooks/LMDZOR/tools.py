3cab572a80b1ef476b1cc542f0ac5ebebebacb39
Pierre Tiengou <ptiengou@spirit2.ipsl.fr>
2024-07-03 09:07:11 +0200

    Update to dev locally

--- python_notebooks/LMDZOR/tools.py
+++ python_notebooks/LMDZOR/tools.py
@@ -18,13 +18,18 @@ wet = ListedColormap(mpl.colormaps['YlGnBu'](np.linspace(0, 1, 10)))
 
 rivers = cartopy.feature.NaturalEarthFeature('physical', 'rivers_lake_centerlines', '10m',edgecolor=(0, 0, 0, 0.3), facecolor='none')
 
+def map_plotvar(plotvar, in_vmin=None, in_vmax=None, in_cmap=myvir, multiplier=1, in_figsize=(9,6.5), in_title=None):
+    fig = plt.figure(figsize=in_figsize)
+    ax = plt.axes(projection=ccrs.PlateCarree())
+    _nice_map(plotvar, ax, in_cmap, in_vmin, in_vmax)
+    plt.title(in_title)
+
 def map_ave(ds, var, in_vmin=None, in_vmax=None, in_cmap=myvir, multiplier=1, in_figsize=(9,6.5)):
     fig = plt.figure(figsize=in_figsize)
     ax = plt.axes(projection=ccrs.PlateCarree())
     plotvar = ds[var].mean(dim='time') * multiplier
     _nice_map(plotvar, ax, in_cmap=in_cmap, in_vmin=in_vmin, in_vmax=in_vmax)
     plt.title(var + ' (' + ds[var].attrs['units'] + ')')
-
 
 def map_diff_ave(ds1, ds2, var, in_vmin=None, in_vmax=None, in_cmap=emb, in_figsize=(9,6.5), sig=False):
     fig = plt.figure(figsize=in_figsize)
@@ -47,7 +52,23 @@ def map_diff_ave(ds1, ds2, var, in_vmin=None, in_vmax=None, in_cmap=emb, in_figs
     _nice_map(diff, ax, in_cmap=in_cmap, in_vmin=in_vmin, in_vmax=in_vmax)
     plt.title(var + ' difference (' + ds1.name + ' - ' + ds2.name + ', ' + ds1[var].attrs['units'] + ')')
 
+def map_rel_diff_ave(ds1, ds2, var, in_vmin=None, in_vmax=None, in_cmap=emb, multiplier=1, in_figsize=(9,6.5)):
+    fig = plt.figure(figsize=in_figsize)
+    ax = plt.axes(projection=ccrs.PlateCarree())
+    rel_diff = ((ds1[var]-ds2[var] + 1E-16) / (ds2[var] + 1E-16)).mean(dim='time') * 100
 
+    _nice_map(rel_diff, ax, in_cmap=in_cmap, in_vmin=in_vmin, in_vmax=in_vmax)
+    plt.title(var + ' relative difference (' + ds1.name + ' - ' + ds2.name + ' ; %)')
+
+def map_two_ds(ds1, ds2, var, in_vmin=None, in_vmax=None, in_cmap=reds, in_figsize=(9,6.5)):
+    fig, axs = plt.subplots(1, 2, figsize=(12, 4), subplot_kw={'projection': ccrs.PlateCarree()})
+    fig.suptitle(var + ' ({})'.format(ds1[var].units))
+    plotvar_1 = ds1[var].mean(dim='time')
+    plotvar_2 = ds2[var].mean(dim='time')
+    _nice_map(plotvar_1, axs[0], in_cmap, in_vmin, in_vmax)
+    _nice_map(plotvar_2, axs[1], in_cmap, in_vmin, in_vmax)
+
+#internal functions
 def _nice_map(plotvar, in_ax, in_cmap, in_vmin, in_vmax):
     in_ax.coastlines()
     in_ax.add_feature(rivers)
